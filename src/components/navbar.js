e2f24a0b2bdb79ece709aac8fee8e25028a88a59
Owen Taylor <you@example.com>
2023-06-28 21:57:36 +0100

    added dark mode and a toggle

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 import NavImage from "./images/NavImage.svg";
 import SunSvg from "./images/sun.svg";
 import MoonSvg from "./images/moon.svg";
-import React, { useState, useRef, useLayoutEffect } from "react";
-
-
-
-
+import SunlightSvg from "./images/sunlight.svg";
+import MoonlightSvg from "./images/moonlight.svg";
+import React, { useState, useRef, useLayoutEffect, useEffect } from "react";
 
 function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const menuRef = useRef(null);
 
   useLayoutEffect(() => {
@@ -20,14 +19,73 @@ function Navbar() {
     }
   }, [isMobileMenuOpen]);
 
+  useEffect(() => {
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setIsDarkMode(false);
+    }
+  }, []);
+
   const closeMobileMenu = () => {
     setIsMobileMenuOpen(false);
+  };
 
-    
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    }
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const getMoonIcon = () => {
+    if (isDarkMode) {
+      return (
+        <img
+          src={MoonlightSvg}
+          className="h-5 w-6 dark:text-white"
+          alt="Moon Icon"
+        />
+      );
+    } else {
+      return (
+        <img
+          src={MoonSvg}
+          className="h-6 w-6 dark:text-white"
+          alt="Moon Icon"
+        />
+      );
+    }
+  };
+
+  const getSunIcon = () => {
+    if (isDarkMode) {
+      return (
+        <img
+          src={SunlightSvg}
+          className="h-6 w-6 dark:text-white"
+          alt="Sun Icon"
+        />
+      );
+    } else {
+      return (
+        <img src={SunSvg} className="h-6 w-6 dark:text-black" alt="Sun Icon" />
+      );
+    }
   };
 
   return (
-    <nav className="bg-gray-100 dark:bg-slate-800 mb-3 fixed top-0 left-0 w-full z-50">
+    <nav
+      className={`bg-gray-100 ${
+        isDarkMode ? "dark:bg-slate-800" : "dark:bg-white"
+      } mb-3 fixed top-0 left-0 w-full z-50`}
+    >
       <div className="max-w-7xl mx-auto md:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center px-4 md:px-0">
@@ -35,7 +93,7 @@ function Navbar() {
               <img src={NavImage} className="h-8" alt="Logo" />
             </Link>
             <Link to="/" onClick={closeMobileMenu}>
-              <h1 className="ml-2 text-xl transition-colors text-black dark:text-white duration-300 hover:text-pink-400">
+              <h1 className="ml-2 text-xl transition-colors text-black dark:text-white duration-300 hover:text-pink-400 dark:hover:text-pink-400">
                 Title
               </h1>
             </Link>
@@ -43,37 +101,61 @@ function Navbar() {
           <div className="hidden md:flex md:items-center md:space-x-4">
             <Link
               to="/"
-              className="block transition-colors duration-300 dark:text-white hover:text-pink-400"
+              className="block transition-colors duration-300 dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
               onClick={closeMobileMenu}
             >
               Blog
             </Link>
             <Link
               to="/map"
-              className="block transition-colors duration-300 dark:text-white hover:text-pink-400"
+              className="block transition-colors duration-300 dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
               onClick={closeMobileMenu}
             >
               Maps
             </Link>
             <Link
               to="/listings"
-              className="block transition-colors duration-300 dark:text-white hover:text-pink-400"
+              className="block transition-colors duration-300 dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
               onClick={closeMobileMenu}
             >
               Listings
             </Link>
             <Link
               to="/profile"
-              className="block transition-colors duration-300 dark:text-white hover:text-pink-400"
+              className="block transition-colors duration-300 dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
               onClick={closeMobileMenu}
             >
               Profile
             </Link>
-          </div>
-          <div className="flex md:hidden items-center px-4 ">
             <button
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-700  hover:bg-gray-200  stroke-black dark:stroke-white dark:hover:bg-slate-600 focus:outline-none focus:ring-3 focus:ring-inset "
+              className={`inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:bg-gray-200 ${
+                isDarkMode
+                  ? "dark:text-white dark:hover:bg-slate-600"
+                  : "dark:text-black"
+              } focus:outline-none focus:ring-3 focus:ring-inset`}
+              onClick={toggleDarkMode}
+            >
+              <span className="sr-only">Toggle Dark Mode</span>
+              {isDarkMode ? getMoonIcon() : getSunIcon()}
+            </button>
+          </div>
+          <div className="flex md:hidden items-center px-4">
+            <button
+              type="button"
+              className={`inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:bg-gray-200 ${
+                isDarkMode
+                  ? "dark:text-white dark:hover:bg-slate-600"
+                  : "dark:text-black"
+              } focus:outline-none focus:ring-3 focus:ring-inset`}
+              onClick={toggleDarkMode}
+            >
+              <span className="sr-only">Toggle Dark Mode</span>
+              {isDarkMode ? getMoonIcon() : getSunIcon()}
+            </button>
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:bg-gray-200 stroke-black dark:stroke-white dark:hover:bg-slate-600 focus:outline-none focus:ring-3 focus:ring-inset"
               aria-expanded="false"
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
@@ -132,33 +214,32 @@ function Navbar() {
             <div className="flex flex-col items-end">
               <Link
                 to="/blog"
-                className="mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 "
+                className="mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
                 onClick={closeMobileMenu}
               >
                 Blog
               </Link>
               <Link
                 to="/map"
-                className=" mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 "
+                className="mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
                 onClick={closeMobileMenu}
               >
                 Maps
               </Link>
               <Link
                 to="/listings"
-                className=" mt-1  block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 "
+                className="mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
                 onClick={closeMobileMenu}
               >
                 Listings
               </Link>
               <Link
                 to="/profile"
-                className=" mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 "
+                className="mt-1 block transition-colors duration-200 text-black dark:text-white hover:text-pink-400 dark:hover:text-pink-400"
                 onClick={closeMobileMenu}
               >
                 Profile
               </Link>
-
             </div>
           </div>
         </div>
